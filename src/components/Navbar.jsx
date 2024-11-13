2d79379f663b64d30455d180aed3b97d5190f5ad
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-11-13 16:28:57 -0500

    Setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+export default function Navbar() {
+  return (
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Projects</a></li>
+        </ul>
+    </nav>
+  )
+}
