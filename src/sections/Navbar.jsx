de2143a8a8f4f3d5e93709d4785b4e3b83aaa9b6
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-11-18 17:04:09 -0500

    Add event listener to sidebar to close when clicked outside

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 
 function NavItems({ pages }) {
@@ -24,9 +24,32 @@ function NavItems({ pages }) {
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
+  const sidebarRef = useRef()
+
+  useEffect(() => {
+    function handleClickOutside(e) {
+      if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
+        setIsOpen(false)
+      }
+    }
+    
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      window.removeEventListener("resize", handleResize);
+    }
+  }, []);
 
   function toggleMenu() {
     setIsOpen((prevState) => !prevState)
+    console.log(isOpen)
   }
 
   const pages = [
@@ -38,34 +61,39 @@ export default function Navbar() {
   ];
 
   return (
-    <header>
-      <div className=''>
+    <header ref={sidebarRef}>
+        {/* Mobile menu button */}
         <div className='fixed top-0 left-0 z-50'>
           <button
             onClick={toggleMenu}
-            className='text-neutral-400 hover:text-white focus:outline-none lg:hidden flex p-4'
+            className={`transform duration-100 ease-in-out ${isOpen ? 'translate-x-[150%]' : 'translate-x-0' } text-neutral-400 hover:text-white focus:outline-none lg:hidden flex p-4`}
             aria-label='toggle menu'
           >
             <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} alt="toggle" className='w-12 h-12' />
           </button>
         </div>
 
-        <div className="lg:flex hidden fixed h-full border-r-4 border-blue-900 px-2 bg-gradient-to-r
-      from-slate-800 to-slate-950">
+        {/* Sidebar */}
+        <div className={`lg:translate-x-0 ${isOpen ? "translate-x-0" : "translate-x-[-100%]"}  transition-all duration-300 ease-in-out fixed h-full border-r-4 border-blue-900 px-2 bg-gradient-to-r after:first:from-slate-800 to-slate-950`}>
           <div className="flex flex-col justify-center items-center h-full">
-            <nav className="h-full p-6 flex flex-col justify-center items-center">
+            <nav className="h-screen p-6 flex flex-col justify-center items-center">
               <NavItems pages={pages} />
             </nav>
           </div>
         </div>
-      </div>
 
-      <div className={`fixed top-0 left-0 w-full transition-all duration-300 ease-in-out 
+      {/* <div className={`fixed top-0 left-0 w-full transition-all duration-300 ease-in-out 
       ${isOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0'} overflow-hidden lg:hidden`}>
         <div className='bg-gradient-to-r from-slate-800 to-slate-950'>
-          Mobile Nav
+          <ul>
+            {pages.map((item) => (
+              <NavLink key={item.location} to={item.location}>
+                <li key={item.location}></li>
+              </NavLink>
+            ))}
+          </ul>
         </div>
-      </div>  
+      </div>   */}
     </header>
   )
 }
